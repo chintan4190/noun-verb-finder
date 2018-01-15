9806ddc811ed374911e6154c253c4fa5fa3934f0
vi19to <chintan.champaklal.doshi@ing.nl>
2018-01-15 12:14:41 +0100

    intial set up

---
+++ src/main/java/com/aigent/nounverbfinder/NounVerbFinderApplication.java
@@ -0,0 +1,12 @@
+package com.aigent.nounverbfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NounVerbFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NounVerbFinderApplication.class, args);
+	}
+}
